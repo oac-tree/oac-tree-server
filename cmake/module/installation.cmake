79bdbb833c25468100b72c087403c0dc6e24bfe7
Walter Van Herck <walter.vanherck@iter.org>
2023-11-08 12:19:48 +0000

    Rename to sup-auto-server and fix small config issues

--- cmake/module/installation.cmake
+++ cmake/module/installation.cmake
@@ -4,44 +4,44 @@
 # https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
 # -----------------------------------------------------------------------------
 
-set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/sup-automation-server)
+set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/sup-auto-server)
 
 # exporting targets to a script and installing it
-install(EXPORT sup-automation-server-targets FILE sup-automation-server-targets.cmake NAMESPACE sup-automation-server:: DESTINATION ${INSTALL_CONFIGDIR})
+install(EXPORT sup-auto-server-targets FILE sup-auto-server-targets.cmake NAMESPACE sup-auto-server:: DESTINATION ${INSTALL_CONFIGDIR})
 
 # -----------------------------------------------------------------------------
 # Exporting
 # -----------------------------------------------------------------------------
 
 # Add all targets to the build-tree export set
-export(TARGETS sup-automation-server NAMESPACE sup-automation-server:: FILE "${PROJECT_BINARY_DIR}/sup-automation-server-targets.cmake")
+export(TARGETS sup-auto-server NAMESPACE sup-auto-server:: FILE "${PROJECT_BINARY_DIR}/sup-auto-server-targets.cmake")
 
 # Export the package for use from the build-tree (goes to $HOME/.cmake)
 if(COA_EXPORT_BUILD_TREE)
   set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
-  export(PACKAGE sup-automation-server)
+  export(PACKAGE sup-auto-server)
 endif()
 
 # -----------------------------------------------------------------------------
-# Creating and installing sup-automation-server-config.cmake
+# Creating and installing sup-auto-server-config.cmake
 # -----------------------------------------------------------------------------
 
 include(CMakePackageConfigHelpers)
 
 # to use in the build tree
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/../config/sup-automation-server-config.cmake.in
-    ${CMAKE_CURRENT_BINARY_DIR}/sup-automation-server-config.cmake
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/../config/sup-auto-server-config.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/sup-auto-server-config.cmake
     INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
 )
 
 # to use in install tree
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-automation-server-config.cmake DESTINATION ${INSTALL_CONFIGDIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-auto-server-config.cmake DESTINATION ${INSTALL_CONFIGDIR})
 
 # -----------------------------------------------------------------------------
-# Create and install sup-automation-server-config-version.cmake file
+# Create and install sup-auto-server-config-version.cmake file
 # -----------------------------------------------------------------------------
 
-write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/sup-automation-server-config-version.cmake VERSION
+write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/sup-auto-server-config-version.cmake VERSION
     ${PROJECT_VERSION} COMPATIBILITY AnyNewerVersion)
 
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-automation-server-config-version.cmake DESTINATION ${INSTALL_CONFIGDIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-auto-server-config-version.cmake DESTINATION ${INSTALL_CONFIGDIR})
--- doc/index.rst
+++ doc/index.rst
@@ -1,12 +1,12 @@
-.. sup-automation-server documentation master file, created by
+.. sup-auto-server documentation master file, created by
    sphinx-quickstart on Thu Jan 20 15:49:09 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to sup-automation-server's documentation!
-===================================
+Welcome to sup-auto-server's documentation!
+===========================================
 
-**sup-automation-server** is a C++ library for SUP automation servers.
+**sup-auto-server** is a C++ library for SUP automation servers.
 
 .. toctree::
    :maxdepth: 2
