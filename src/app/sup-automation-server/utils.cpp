a391e20ff111ac678e2d99276d3a2a65c2ab562b
Walter Van Herck <Walter.Vanherck@iter.org>
2024-05-29 15:33:41 +0000

    Add temporary fixed procedure for testing

--- src/app/sup-automation-server/utils.cpp
+++ src/app/sup-automation-server/utils.cpp
@@ -21,6 +21,8 @@
 
 #include "utils.h"
 
+#include <sup/sequencer/sequence_parser.h>
+
 namespace sup
 {
 namespace auto_server
@@ -28,10 +30,27 @@ namespace auto_server
 namespace utils
 {
 
+const std::string kSimpleCounterProcedureString{
+R"RAW(<?xml version="1.0" encoding="UTF-8"?>
+<Procedure>
+  <Sequence>
+    <Copy inputVar="one" outputVar="var1"/>
+    <Copy inputVar="one" outputVar="var2"/>
+  </Sequence>
+  <Workspace>
+    <Local name="one" type='{"type":"uint32"}' value='1'/>
+    <Local name="var1" type='{"type":"uint32"}' value='0'/>
+    <Local name="var2" type='{"type":"uint32"}' value='0'/>
+  </Workspace>
+</Procedure>
+)RAW"};
+
 AutomationServer::ProcedureList GetProcedureList(sup::cli::CommandLineParser& parser)
 {
   (void)parser;
-  return {};
+  AutomationServer::ProcedureList result;
+  result.emplace_back(sup::sequencer::ParseProcedureString(kSimpleCounterProcedureString));
+  return result;
 }
 
 }  // namespace utils
