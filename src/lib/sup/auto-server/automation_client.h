03a5fa897cf6ea9d850a711c430c26e3e8ecb14b
Walter Van Herck <Walter.Vanherck@iter.org>
2024-08-09 12:49:37 +0000

    Doxygen comments for AutomationClient

--- src/lib/sup/auto-server/automation_client.h
+++ src/lib/sup/auto-server/automation_client.h
@@ -35,15 +35,44 @@ namespace sup
 namespace auto_server
 {
 /**
- * @brief AutomationClient.
+ * @brief AutomationClient wraps an IJobManager implementation and allows to connect custom
+ * IJobInfoIO implementations to jobs that are managed by the IJobManager.
  */
 class AutomationClient
 {
 public:
+  /**
+   * @brief AutomationClient constructor with injection of the wrapped IJobManager implementation
+   * and a factory function to allow connecting custom IJobInfoIO implementations to the managed
+   * jobs.
+   *
+   * @param job_manager IJobManager implementation that provides static information about multiple jobs.
+   * @param factory_func Factory function to use for the creation of listener objects.
+   */
   AutomationClient(IJobManager& job_manager, const ListenerFactoryFunction& factory_func);
+
+  /**
+   * @brief Destructor.
+   */
   ~AutomationClient();
 
+  /**
+   * @brief Connect status updates and user input/output of a remote job to a custom provided
+   * IJobInfoIO implementation.
+   *
+   * @param job_idx Index of job to connect.
+   * @param job_info_io Custom IJobInfoIO implementation.
+   * @return True when successful. Failure typically indicates that the job was already connected or
+   * the client could not retrieve the required information from the IJobManager.
+   */
   bool Connect(std::size_t job_idx, IJobInfoIO& job_info_io);
+
+  /**
+   * @brief Disconnect a previously connected IJobInfoIO implementation.
+   *
+   * @param job_idx Index of job to disconnect.
+   * @return True when successful. Failure typically means the job was not connected previously.
+   */
   bool Disconnect(std::size_t job_idx);
 
 private:
