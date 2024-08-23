83ae91ecc78237542665d59417660506fec511a9
Walter Van Herck <Walter.Vanherck@iter.org>
2024-08-23 15:36:25 +0000

    Add input request structure

---
+++ src/lib/sup/auto-server/anyvalue_input_request.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - AUTOMATION-SERVER
+ *
+ * Description   : SUP automation server
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_AUTO_SERVER_ANYVALUE_INPUT_REQUEST_H_
+#define SUP_AUTO_SERVER_ANYVALUE_INPUT_REQUEST_H_
+
+#include <sup/dto/anyvalue.h>
+
+#include <string>
+#include <vector>
+
+namespace sup
+{
+namespace auto_server
+{
+
+const std::string kInputRequestOptionsFieldName = "options";
+const std::string kInputRequestMetadataFieldName = "metadata";
+
+/**
+ * @brief Enumeration for the different types of user input.
+ *
+ */
+enum class InputRequestType
+{
+  kUserValue = 0,
+  kUserChoice
+};
+
+/**
+ * @brief e.
+ */
+struct AnyValueInputRequest
+{
+public:
+  InputRequestType m_request_type;
+  sup::dto::AnyValue m_meta_data;
+  sup::dto::AnyType m_input_type;
+};
+
+AnyValueInputRequest CreateUserValueRequest(sup::dto::AnyValue& value,
+                                            const std::string& description);
+
+AnyValueInputRequest CreateUserChoiceRequest(const std::vector<std::string>& options,
+                                             const sup::dto::AnyValue& metadata);
+
+}  // namespace auto_server
+
+}  // namespace sup
+
+#endif  // SUP_AUTO_SERVER_ANYVALUE_INPUT_REQUEST_H_
---
+++ src/lib/sup/auto-server/base/anyvalue_input_request.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - AUTOMATION-SERVER
+ *
+ * Description   : SUP automation server
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/auto-server/anyvalue_input_request.h>
+
+namespace sup
+{
+namespace auto_server
+{
+
+AnyValueInputRequest CreateUserValueRequest(sup::dto::AnyValue& value,
+                                            const std::string& description)
+{
+  sup::dto::AnyValue meta_data = description;
+  AnyValueInputRequest input_request{ InputRequestType::kUserValue, meta_data, value.GetType() };
+  return input_request;
+}
+
+AnyValueInputRequest CreateUserChoiceRequest(const std::vector<std::string>& options,
+                                             const sup::dto::AnyValue& metadata)
+{
+  sup::dto::AnyValue options_av{ options.size(), sup::dto::StringType };
+  for (std::size_t idx = 0; idx < options.size(); ++ idx)
+  {
+    options_av[idx] = options[idx];
+  }
+  sup::dto::AnyValue meta = {{
+    { kInputRequestOptionsFieldName, options_av },
+    { kInputRequestMetadataFieldName, metadata }
+  }};
+  AnyValueInputRequest input_request{ InputRequestType::kUserChoice, meta,
+                                      sup::dto::SignedInteger32Type};
+  return input_request;
+}
+
+}  // namespace auto_server
+
+}  // namespace sup
