c1ef22e02e9e5222ec0e0636cddd90d369c64374
torresr <rjtorres@criticalsoftware.com>
2025-01-27 14:19:37 +0000

    Fix cmake library names

--- cmake/module/installation.cmake
+++ cmake/module/installation.cmake
@@ -13,14 +13,14 @@ set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/oac-tree-server-shared)
 # -----------------------------------------------------------------------------
 # Exporting targets
 # -----------------------------------------------------------------------------
-set(PACKAGE_TARGETS_FILENAME oac-tree-server-targets.cmake)
+set(PACKAGE_TARGETS_FILENAME oac-tree-server-shared-targets.cmake)
 set(PACKAGE_TARGETS_FILE ${BUILD_CONFIGDIR}/${PACKAGE_TARGETS_FILENAME})
 
 # Install the export targets for installation usage, this does not relate to the build tree file
-install(EXPORT oac-tree-server-targets FILE ${PACKAGE_TARGETS_FILENAME} NAMESPACE oac-tree-server:: DESTINATION ${INSTALL_CONFIGDIR})
+install(EXPORT oac-tree-server-shared-targets FILE ${PACKAGE_TARGETS_FILENAME} NAMESPACE oac-tree-server-shared:: DESTINATION ${INSTALL_CONFIGDIR})
 
 # Generate the export targets for the build tree usage
-export(TARGETS oac-tree-server-shared NAMESPACE oac-tree-server:: FILE ${PACKAGE_TARGETS_FILE})
+export(TARGETS oac-tree-server-shared NAMESPACE oac-tree-server-shared:: FILE ${PACKAGE_TARGETS_FILE})
 
 # Export the package to CMake registry for build tree usage (goes to $HOME/.cmake)
 if(COA_EXPORT_BUILD_TREE)
@@ -31,7 +31,7 @@ endif()
 # -----------------------------------------------------------------------------
 # Version configuration
 # -----------------------------------------------------------------------------
-set(PACKAGE_VERSION_FILE ${BUILD_CONFIGDIR}/oac-tree-server-config-version.cmake)
+set(PACKAGE_VERSION_FILE ${BUILD_CONFIGDIR}/oac-tree-server-shared-config-version.cmake)
 
 # Generate the version config file, shared in both build tree and installation usage
 write_basic_package_version_file(${PACKAGE_VERSION_FILE} COMPATIBILITY AnyNewerVersion)
@@ -41,7 +41,7 @@ install(FILES ${PACKAGE_VERSION_FILE} DESTINATION ${INSTALL_CONFIGDIR})
 # -----------------------------------------------------------------------------
 # Package configuration
 # -----------------------------------------------------------------------------
-set(PACKAGE_CONFIG_FILE ${BUILD_CONFIGDIR}/oac-tree-server-config.cmake)
+set(PACKAGE_CONFIG_FILE ${BUILD_CONFIGDIR}/oac-tree-server-shared-config.cmake)
 
 # Generate the package config file, shared in both build tree and installation usage
 write_package_config_file(oac-tree-server-shared OUTPUT ${PACKAGE_CONFIG_FILE} INSTALL_DESTINATION ${INSTALL_CONFIGDIR} DEPENDENCIES oac-tree sup-protocol)
